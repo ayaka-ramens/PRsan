3a19d7d9762260c7ddbd6bb395f7943333d9d6f0
Ayaka Moronaga <a.moronaga@everyleaf.com>
2021-09-30 09:28:33 +0900

    不要なメソッドを削除

--- lib/PRsan/tweet.rb
+++ lib/PRsan/tweet.rb
@@ -17,20 +17,5 @@ module PRsan
       # TODO: エラーハンドリング
       response = Twitter::REST::Request.new(@twitter_client, :get, url).perform
     end
-
-    def like_count(data)
-      # response[:data][0].dig(:public_metrics, :like_count)という感じで配列から取り出す必要があるが
-      # 配列の中身だけ引数で受け取ることで
-      # data..dig(:public_metrics, :like_count) みたいにならないかなそれならいちいちメソッドいらんかも
-    end
-
-    def user_profile_clicks
-    end
-
-    def impression_count
-    end
-
-    def url_link_clicks
-    end
   end
 end
