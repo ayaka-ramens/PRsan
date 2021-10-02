7d14d3e3c52fe6af0aabaadc96677b09083ada2e
Ayaka Moronaga <a.moronaga@everyleaf.com>
2021-10-02 22:43:14 +0900

    書き込み結果をslack通知するようにした

--- lib/PRsan.rb
+++ lib/PRsan.rb
@@ -1,15 +1,17 @@
 require 'PRsan/version'
 require 'PRsan/tweet'
 require 'PRsan/spreadsheet'
+require 'PRsan/slack'
 
 module PRsan
-  def self.tweet_analysis
+  def self.analysis_tweet
     spreadsheet = Spreadsheet.new
     twitter = Tweet.new
 
     tweet_ids = spreadsheet.target_tweet_ids
     tweets_data = twitter.get_tweets_data(tweet_ids)
-    spreadsheet.write_analysis_data(tweets_data)
+    writed_count = spreadsheet.write_analysis_data(tweets_data)
+    Slack.notify(writed_count)
   end
 
   class Error < StandardError; end
